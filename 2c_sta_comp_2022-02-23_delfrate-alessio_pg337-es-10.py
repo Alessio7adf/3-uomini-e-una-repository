684d4f8c92692d3950f5761b12d4558539c58a00
Alessio7adf <96028053+Alessio7adf@users.noreply.github.com>
2022-02-22 12:58:36 +0100

    Update 2c_sta_comp_2022-02-23_delfrate-alessio_pg337-es-10.py

--- 2c_sta_comp_2022-02-23_delfrate-alessio_pg337-es-10.py
+++ 2c_sta_comp_2022-02-23_delfrate-alessio_pg337-es-10.py
@@ -7,5 +7,10 @@ for i in range(NUMELE):
   vettore.append(random.randint(MIN, MAX))
 
 n = input("Inserire valore: ")
+volte = 0
 
-for i in 
+for i in vettore:
+  if i == n:
+    volte +=1
+
+print("numero di volte in cui il valore appare nel vettore", vettore, ":", volte)
