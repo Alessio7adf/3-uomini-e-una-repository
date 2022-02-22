d73e118184baf3d3b86e7cf44c9c520cebd29767
Alessio7adf <alessio7.adf@gmail.com>
2022-02-22 19:36:26 +0100

    esercizi per il 23

--- 2c_sta_comp_2022-02-23_delfrate-alessio_pg336-es-09.py
+++ 2c_sta_comp_2022-02-23_delfrate-alessio_pg336-es-09.py
@@ -4,9 +4,9 @@ NUMELE, MIN, MAX = 15, 1, 20
 vettore = []
 
 for i in range(NUMELE):
-    n = random.randint(MIN, MAX)
-    while not n in vettore:
-        n = random.randint(MIN, MAX)
-    vettore.append(n)
+	n = random.randint(MIN, MAX)
+	while vettore.__contains__(n):
+		n = random.randint(MIN, MAX)
+	vettore.append(n)
 
 print("vettore:", vettore)
--- 2c_sta_comp_2022-02-23_delfrate-alessio_pg337-es-10.py
+++ 2c_sta_comp_2022-02-23_delfrate-alessio_pg337-es-10.py
@@ -4,13 +4,13 @@ NUMELE, MIN, MAX = 15, 1, 100
 vettore = []
 
 for i in range(NUMELE):
-  vettore.append(random.randint(MIN, MAX))
+	vettore.append(random.randint(MIN, MAX))
 
 n = input("Inserire valore: ")
 volte = 0
 
 for i in vettore:
-  if i == n:
-    volte +=1
+	if i == n:
+		volte += 1
 
 print("numero di volte in cui il valore appare nel vettore", vettore, ":", volte)
