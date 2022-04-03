81f0cb9c018eb31801224d18ab7256a2bc0e3bf1
Alessio7adf <alessio7.adf@gmail.com>
2022-04-03 22:07:48 +0200

    corretto es 16

--- 2c_sta_comp_2022-04-04_delfrate-alessio_pg337-es-16.py
+++ 2c_sta_comp_2022-04-04_delfrate-alessio_pg337-es-16.py
@@ -6,20 +6,20 @@ vettore = []
 for i in range(NUMELE):
 	vettore.append(random.randint(MIN, MAX))
 
-vettore = [5, 2, 17, 12, 8, 15, 2, 14, 12, 11, 8, 9, 4, 3, 13]
+pivot = random.randint(0, NUMELE - 1)
 
-pivot = vettore[random.randint(0, NUMELE - 1)]
-
-pivot = vettore[9]
-
-print("pivot:", pivot)
+print("indice pivot:\t", pivot)
+print("valore pivot:\t", vettore[pivot])
 print("vettore prima:\t", vettore)
 
-for i in range(NUMELE):
+pivot = vettore.pop(pivot)
 
-	if vettore[i] > pivot and i < vettore.index(pivot):
-		vettore.insert(vettore.index(pivot) - 1, vettore.pop(i))
-	elif vettore[i] < pivot and i > vettore.index(pivot):
-		vettore.insert(vettore.index(pivot) + 1, vettore.pop(i))
+vettore.insert(0, pivot)
+
+for i in range(1, NUMELE):
+
+	if vettore[i] <= pivot:
+		vettore.insert(0, vettore.pop(i))
+		i -= 1
 
 print("vettore dopo:\t", vettore)
